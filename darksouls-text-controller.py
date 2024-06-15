745b6d458c8da2aac8eaeba4747b95ee922a5bcb
Jeremy Eudy <jeremyeudy@gmail.com>
2024-06-15 01:05:13 -0400

    Revised method to instead use keyboard listener. This works sometimes? You need to disable all keybinds in the settings, but after that it should work. It now just needs more commands

--- darksouls-text-controller.py
+++ darksouls-text-controller.py
@@ -1,10 +1,8 @@
+import time
 from random import choice
+
 import vgamepad as vg
-
-
-class MultiKeyDict():
-    def __init__(self, **kwargs):
-        self._keys
+from pynput import keyboard
 
 
 class Controller(vg.VX360Gamepad):
@@ -19,14 +17,16 @@ class Controller(vg.VX360Gamepad):
             # ("back", "backstep"): self.backstep,
             # ("sprint", "dash"): self.sprint,
             # ("jump"): self.jump,
-            # ("right", "look right"): self.look_right,
-            # ("left", "look left"): self.look_left,
-            # ("turn"): self.turn,
+            ("right", "look right"): self.right,
+            ("left", "look left"): self.left,
+            ("up", "look up"): self.up,
+            ("down", "look down"): self.down,
+            ("turn"): self.turn,
             # ("lock-on", "lock", "lock on"): self.lock,
-            # ("light attack", "swing", "hit"): self.light_attack,
-            # ("heavy attack", "big swing"): self.heavy_attack,
-            # ("attack"): self.attack,
-            # ("charge attack"): self.charge_attack,
+            ("light attack", "swing", "hit"): self.light_attack,
+            ("heavy attack", "big swing"): self.heavy_attack,
+            ("attack"): self.attack,
+            ("charge attack"): self.charge_attack,
             # ("spell", "magic", "pray"): self.spell,
             # ("interact", "take", "pickup", "grab", "talk", "talk to", "speak"): self.interact,
             # ("use item", "use", "heal"): self.use_item,
@@ -36,6 +36,7 @@ class Controller(vg.VX360Gamepad):
         for shortcuts, method in self.command_map.items():
             if command in shortcuts:
                 method()
+                self.update()
                 return
         print("Invalid command!")
 
@@ -58,19 +59,55 @@ class Controller(vg.VX360Gamepad):
         # BEHOLD MY SIN
         [self.left, self.right][choice([0, 1])]()
 
+    def left(self):
+        self.right_joystick(x_value=-32768, y_value=0)
+
+    def right(self):
+        self.right_joystick(x_value=32768, y_value=0)
+
+    def up(self):
+        self.right_joystick(x_value=0, y_value=32768)
+
+    def down(self):
+        self.right_joystick(x_value=0, y_value=-32768)
+
     def attack(self):
         # BEHOLD MY SIN
         [self.light_attack, self.heavy_attack][choice([0, 1])]()
 
+    def light_attack(self):
+        self.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_SHOULDER)
+        self.update()
+        self.release_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_SHOULDER)
 
-def game_loop():
-    gamepad = Controller()
-    print("Welcome to Game to TRPG, the tool no one asked for which makes your life harder. For FUN!")
-    print("Enter a command below, or type help for a symblance of guidance")
-    while True:
-        command = input(">").lower()
-        if command == "help":
-            print("""
+    def heavy_attack(self):
+        self.right_trigger(value=255)
+        self.update()
+        self.right_trigger(value=0)
+
+    def charge_attack(self):
+        self.right_trigger(value=255)
+        self.update()
+        time.sleep(5)
+        self.right_trigger(value=255)
+
+
+COMMAND = []
+CONTROLLER = Controller()
+
+
+def on_press(key):
+    global COMMAND
+    global CONTROLLER
+    try:
+        COMMAND.append(key.char)
+    except AttributeError:
+        if key == keyboard.Key.space:
+            COMMAND.append(" ")
+        elif key == keyboard.Key.enter:
+            str_command = "".join(COMMAND)
+            if str_command == "help":
+                print("""
 Commands:
     - Movement:
         - forward: run, go
@@ -82,8 +119,10 @@ Commands:
         - jump
 
     - View:
-        - right: turn (50% chance)
-        - left: turn (50% chance)
+        - right: look right, turn (50% chance)
+        - left: look left, turn (50% chance)
+        - up: look up
+        - down: look down
         - lock-on: lock, lock on
 
     - Combat:
@@ -94,10 +133,30 @@ Commands:
     - General:
         - interact: take, pickup, grab, talk, talk to, speak
         - use item: use, heal
-            """)
-            continue
-        gamepad.execute_command(command)
+                """)
+            elif str_command == "quit" or str_command == "exit":
+                print("Thank you for ending your suffering")
+                return False
+            else:
+                print(f"Submitted command: {str_command}")
+                CONTROLLER.execute_command(str_command)
+            COMMAND = []
+        elif key == keyboard.Key.esc:
+            return False
+
+
+def splash():
+    print("Welcome to Game to TRPG, the tool no one asked for which makes your life harder. For FUN!")
+    print("Enter a command below, or type help for a semblance of guidance")
 
 
 if __name__ == "__main__":
-    game_loop()
+    splash()
+    with keyboard.Listener(
+        on_press=on_press
+    ) as listener:
+        listener.join()
+    # listener = keyboard.Listener(
+    #     on_press=on_press
+    # )
+    # listener.start()
